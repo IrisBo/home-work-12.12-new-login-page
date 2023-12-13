058235596c53467fd8bc867b7928b641dfc03c40
Iris Bolocan <nexuss13@gmail.com>
2023-12-13 21:55:56 +0200

    added changes

--- scrip2_help.js
+++ scrip2_help.js
@@ -1,24 +1,3 @@
-
-// function test (){
-//     document.write("dbvkhdbvkh")
-// }
-function checkDetails() {
-
-    const username1 = document.querySelector(".email_1").value
-    const password1 = document.querySelector(".password_1").value
-
-    if (username1 === "idanmagl@gmail.com" && password1 === "1234$$") {
-
-        window.location.href = "http://127.0.0.1:5500/my_page.html";
-
-    }
-    else {
-
-        alert("username or password incorrect")
-
-    }
-}
-
 function checkPassword() {
     const inputValue = document.getElementById("passwordInput").value;
     const messageElement = document.getElementById("message");
