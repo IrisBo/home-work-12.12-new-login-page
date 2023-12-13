6360e9ccb28432a58561b6b621e3a711403f5c20
Iris Bolocan <nexuss13@gmail.com>
2023-12-13 14:17:17 +0200

    added changes

--- scrips.js
+++ scrips.js
@@ -7,7 +7,7 @@ function checkDetails() {
     const username1 = document.querySelector(".email_1").value
     const password1 = document.querySelector(".password_1").value
 
-    if (username1 == "1@gmail" && password1 == "1234") {
+    if (username1 === "1@" && password1 === "1234") {
 
         window.location.href = "http://127.0.0.1:5500/my_page.html";
 
@@ -19,3 +19,18 @@ function checkDetails() {
     }
 }
 
+function getPassword() {
+
+    const username1 = document.querySelector(".text_1").value
+    let password = document.querySelector("#my_password")
+    if (username1 === "1@") {
+        password.textContent = "1234"
+        console.log(password)
+    }
+    else {
+
+        alert("try again")
+
+    }
+
+}
